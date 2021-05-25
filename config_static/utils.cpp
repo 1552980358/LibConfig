9fc6e9d1afbab25ffa2a0ca5b978cf1e83696b67
1552980358 <1552980358@qq.com>
2021-05-25 20:56:25 +0800

    utils: Fix str_starts_with and str_ends_with logical mistake
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- config_static/utils.cpp
+++ config_static/utils.cpp
@@ -5,7 +5,7 @@ bool str_starts_with(const string &str, char c) {
     // This is for cywin
     return str.starts_with(c)
 #endif
-    return str.empty() && str.find(c) == 0;
+    return !str.empty() && str.find(c) == 0;
 }
 
 bool str_ends_with(const string &str, char c) {
@@ -13,5 +13,5 @@ bool str_ends_with(const string &str, char c) {
     // This is for cywin
     return str.ends_with(c);
 #endif
-    return str.empty() && str.find_last_of(c) == str.size() - 1;
+    return !str.empty() && str.find_last_of(c) == str.size() - 1;
 }
