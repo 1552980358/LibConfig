49cf2d910d8dbacafbaee3c0576459a2577a8462
1552980358 <1552980358@qq.com>
2021-05-27 20:45:58 +0800

    libconfig: Remove redundant read_config() methods
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- config_static/libconfig.cpp
+++ config_static/libconfig.cpp
@@ -1,11 +1,3 @@
 #include "libconfig.h"
 
 #include <iostream>
-
-config_file *read_config(const char *c_str) {
-    return new config_file(c_str);
-}
-
-config_file *read_config(const string &str) {
-    return new config_file(str);
-}
--- config_static/libconfig.h
+++ config_static/libconfig.h
@@ -3,8 +3,4 @@
 
 #include "config_file.h"
 
-config_file *read_config(const char *);
-
-config_file *read_config(const string &);
-
 #endif //LIBCONFIG_LIBCONFIG_H
