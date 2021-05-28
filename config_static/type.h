2f308bf66d138684db478b08db64166301299c34
1552980358 <1552980358@qq.com>
2021-05-28 14:51:57 +0800

    Update type.h
    Signed-off-by: 1552980358 <1552980358@qq.com>

---
+++ config_static/type.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_STATIC_TYPE_H
+#define CONFIG_STATIC_TYPE_H
+
+typedef void *config_file_t;
+
+typedef void *config_part_t;
+
+typedef void *config_value_t;
+
+#endif //CONFIG_STATIC_TYPE_H
